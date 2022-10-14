9d69560cc2d476aacb1056720b440664c9520b41
SamuelPoff <92762777+SamuelPoff@users.noreply.github.com>
2022-10-14 15:02:15 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  placesApiKey: "AIzaSyDlmQzxwVe4mT3a-KpyUxCSjipWkJd3OS4",
+  weatherapiKey: "70fb6d4c8d1d4e15847235155221208",
+
+  cacheTimeout: 43200000, //43200000 = 12 Hours in milliseconds
 };
